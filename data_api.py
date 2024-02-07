81d8c8442d96ce3c14997676a6cdae71a4e38373
kacembouden <87078603+kacembouden@users.noreply.github.com>
2024-02-07 18:51:22 +0100

    Update data_api.py

--- data_api.py
+++ data_api.py
@@ -80,4 +80,4 @@ if __name__ == '__main__':
  
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run(port = "443")
+    app.run(port = "3000")
