c58a1e7d3016c34caa4536d311809aa35f7de9d7
kacembouden <87078603+kacembouden@users.noreply.github.com>
2024-02-08 16:31:58 +0100

    Update data_api.py

--- data_api.py
+++ data_api.py
@@ -21,15 +21,15 @@ def ohlc(symbol,timeframe):
         mt5.shutdown()
 
     last = request.args.get('last', type=int)
-    fromm = request.args.get('from', type=str)
+    since = request.args.get('since', type=str)
     to = request.args.get('to', type=str)
 
     if last != None:  
         btc_rates = mt5.copy_rates_from_pos(symbol, tf[timeframe], 0, last)
-    elif fromm :
-        fromm = fromm.split('-')
+    elif since :
+        since = since.split('-')
         to = to.split('-')
-        year_f, month_f, day_f, hour_f = fromm[0], fromm[1],fromm[2],fromm[3]
+        year_f, month_f, day_f, hour_f = since[0], since[1],since[2],since[3]
         year_t, month_t, day_t, hour_t = to[0] , to[1], to[2], to[3]
         btc_rates = mt5.copy_rates_range(symbol, tf[timeframe], datetime(int(year_f),int(month_f),int(day_f),int(hour_f)),
                                         datetime(int(year_t),int(month_t),int(day_t),int(hour_t)))
@@ -52,17 +52,20 @@ def tick(symbol):
         mt5.shutdown()
 
     last = request.args.get('last', type=int)
-    fromm = request.args.get('from', type=str)
+    since = request.args.get('since', type=str)
     to = request.args.get('to', type=str)
 
 
     if last != None: 
         btc_rates = mt5.copy_ticks_from(symbol, datetime(2025, 2, 6), last, mt5.COPY_TICKS_ALL) 
-    elif fromm :
-        fromm = fromm.split('-')
+    elif since :
+        since = since.split('-')
         to = to.split('-')
-        year_f, month_f, day_f, hour_f = fromm[0], fromm[1],fromm[2],fromm[3]
+
+        year_f, month_f, day_f, hour_f = since[0], since[1],since[2],since[3]
         year_t, month_t, day_t, hour_t = to[0] , to[1], to[2], to[3]
+
+
         btc_rates = mt5.copy_ticks_range(symbol, datetime(int(year_f),int(month_f),int(day_f),int(hour_f)),
                                         datetime(int(year_t),int(month_t),int(day_t),int(hour_t)) , mt5.COPY_TICKS_ALL)
 
@@ -74,6 +77,7 @@ def tick(symbol):
 
 
     return dumps(parsed, indent=4)
+
     
 
 if __name__ == '__main__':
